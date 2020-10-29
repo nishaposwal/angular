984fa1f96e4baf75cb47c69ca48f01587c60743d
nishaposwal <nishaposwal321@gmail.com>
2020-10-29 18:22:55 +0530

    Add routing to the project

--- game/src/app/app.module.ts
+++ game/src/app/app.module.ts
@@ -2,20 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { BoxComponent } from './box/box.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
+
+const routes: Routes = [
+  {
+    path: '' , component: HomeComponent
+  },
+  {
+     path: 'game' , component: BoardComponent
+  }
+
+];
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    BoxComponent
+    BoxComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
