8eb46904c55fcf5142f8724ef8aa16fd0a46e04a
Nisha Lal <Nisha.Lal@ril.com>
2020-10-29 16:56:50 +0530

    Add implemantation of the game

---
+++ game/src/app/box/box.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.css']
+})
+export class BoxComponent {
+
+  @Input() value : 'X' | 'O';
+ 
+}
+
+
+  
+ 
+
+
+
