b297cb00a9bdb5d8ab85db8e8856cda3ff060be4
nishaposwal <nishaposwal321@gmail.com>
2020-10-30 10:56:46 +0530

    Add blessing's template

--- game/src/app/app.module.ts
+++ game/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
@@ -42,6 +42,7 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes),
+    FormsModule
   ],
   exports: [
     RouterModule,
